3a548c9a2ea93a805403e6bba5168179aeac5a1c
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-15 03:39:35 -0800

    Update Currency.h
    defined currency.h

--- Currency.h
+++ Currency.h
@@ -1,1 +1,13 @@
+#ifndef CURRENCY_H
+#define CURRENCY_H
 
+class Currency{
+  public:
+          string currencyname;  /*name of currency */
+          int whole part;       /* whole part */
+          int fractional part;
+          string fractional name; /* name of currency fractional part */
+}
+
+#endif
+   
