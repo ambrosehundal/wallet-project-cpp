54ea1df6216e635957b0be0f2283fa7f08c2ff13
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-16 01:43:45 -0800

    Update Yen.h

--- Yen.h
+++ Yen.h
@@ -1,1 +1,30 @@
+#ifndef YEN_H
+#define YEN_H
+#include "Currency.h"
 
+using namespace std;
+
+
+//Inherit all currency attributes from Currency class
+class Yen: public Currency {
+public:
+      //constructors
+      Yen();  
+      Yen(): Currency("Yen", "sen")
+        
+//Update fractional parts into whole parts
+void CurrencyPart();
+  
+      //Overloaded operators
+      Yen operator + (const Yen &);
+      Yen operator + (const double);
+      Yen operator - (const Yen &);
+      Yen operator - (const double);
+      Yen operator = (const Yen &);
+      Yen operator = (const double);
+
+   friend std::ostream& operator << (std::ostream &, const Yen &);
+   friend std::istream& operator >> (std::istream &, Yen &);
+};
+
+#endif
