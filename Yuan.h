e73d7f7259e8275f8ca99af5014958a014abef59
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-16 01:46:14 -0800

    Update Yuan.h

--- Yuan.h
+++ Yuan.h
@@ -1,1 +1,30 @@
+#ifndef YUAN_H
+#define YUAN_H
+#include "Currency.h"
 
+using namespace std;
+
+
+//Inherit all currency attributes from Currency class
+class Yuan: public Currency {
+public:
+      //constructors
+      Yuan();  
+      Yuan(): Currency("Yuan", "fen")
+        
+//Update fractional parts into whole parts
+void CurrencyPart();
+  
+      //Overloaded operators
+      Yuan operator + (const Yuan &);
+      Yuan operator + (const double);
+      Yuan operator - (const Yuan &);
+      Yuan operator - (const double);
+      Yuan operator = (const Yuan &);
+      Yuan operator = (const double);
+
+   friend std::ostream& operator << (std::ostream &, const Yuan &);
+   friend std::istream& operator >> (std::istream &, Yuan &);
+};
+
+#endif
