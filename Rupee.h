9f2c1b1b94dcc5042137a01cb036d09820057de0
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-16 01:40:17 -0800

    Update Rupee.h

--- Rupee.h
+++ Rupee.h
@@ -1,1 +1,30 @@
+#ifndef RUPEE_H
+#define RUPEE_H
+#include "Currency.h"
 
+using namespace std;
+
+
+//Inherit all currency attributes from Currency class
+class Rupee: public Currency {
+public:
+      //constructors
+      Rupee();  
+      Rupee(): Currency("Rupee", "paise")
+        
+//Update fractional parts into whole parts
+void CurrencyPart();
+  
+      //Overloaded operators
+      Rupee operator + (const Rupee &);
+      Rupee operator + (const double);
+      Rupee operator - (const Rupee &);
+      Rupee operator - (const double);
+      Rupee operator = (const Rupee &);
+      Rupee operator = (const double);
+
+   friend std::ostream& operator << (std::ostream &, const Rupee &);
+   friend std::istream& operator >> (std::istream &, Rupee &);
+};
+
+#endif
