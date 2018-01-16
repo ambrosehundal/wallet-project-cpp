9db52c043bc7c43c5aef4e3695f12c0a106b922b
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-16 01:35:22 -0800

    Update Dollar.h
    Dollar.h defined, check again on implementing constructors

--- Dollar.h
+++ Dollar.h
@@ -1,1 +1,33 @@
+#ifndef DOLLAR_H
+#define DOLLAR_H
+#include "Currency.h"
+
+using namespace std;
+
+
+//Inherit all currency attributes from Currency class
+class Dollar: public Currency {
+public:
+      //constructors
+      Dollar();  
+      Dollar(): Currency("Dollar", "cent")
+        
+//Update fractional parts into whole parts
+void CurrencyPart();
+  
+      //Overloaded operators
+      Dollar operator + (const Dollar &);
+      Dollar operator + (const double);
+      Dollar operator - (const Dollar &);
+      Dollar operator - (const double);
+      Dollar operator = (const Dollar &);
+      Dollar operator = (const double);
+
+   friend std::ostream& operator << (std::ostream &, const Dollar &);
+   friend std::istream& operator >> (std::istream &, Dollar &);
+};
+
+#endif
+      
+  
 
