b0b45dd0e44bcaeb68180afa11721ebadcd4c898
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-21 19:49:19 -0800

    Update Rupee.cpp

--- Rupee.cpp
+++ Rupee.cpp
@@ -1,1 +1,17 @@
+#include "Currency.h"
+#include "Rupee.h"
 
+void Rupee::setUp()
+{
+	currencyname = "Rupees";
+	fractionalname = "Paises";
+	partsPerwhole = 100;
+}
+Rupee::Rupee(int initWhole, int initFractional)
+{
+	setUp();
+}
+Rupee::~Rupee()
+{
+
+}
