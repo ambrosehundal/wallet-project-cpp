9a14bf470d3ff71dacd519841c24f7c9a7a76118
Ambrose Hundal <amshunda@ucsc.edu>
2018-01-21 19:51:02 -0800

    Update Yen.cpp

--- Yen.cpp
+++ Yen.cpp
@@ -1,1 +1,16 @@
+#include "Currency.h"
+#include "Yen.h"
+Yen::Yen(int initWhole, int initFractional)
+{
+	setUp();
+}
+Yen::~Yen()
+{
 
+}
+void Yen::setUp()
+{
+	currencyname = "Yens";
+	fractionalname = "Sens";
+	partsPerwhole = 100;
+}
